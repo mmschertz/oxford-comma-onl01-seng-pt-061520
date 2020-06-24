ad80ae7387529aec0eb66afb9ac3ca6ef90c8492
Megan Schertz <meganschertz@gmail.com>
2020-06-24 18:22:34 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
-end
+    if array.size <= 2
+        return array.join(' and ')
+    else array.size < 3 
+   remove = array.pop
+   array.join(", ").to_s << (", and #{remove}")
+    end
+  end
